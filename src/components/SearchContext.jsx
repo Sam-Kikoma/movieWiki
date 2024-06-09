c7ce52acc744473e53f4eb17c751e68dcd85f9e1
Sam Kikoma <kikomasamson@gmail.com>
2024-06-09 19:24:13 +0200

    useContext

---
+++ src/components/SearchContext.jsx
@@ -0,0 +1,11 @@
+import { createContext, useState } from "react";
+
+export const SearchContext = createContext();
+export const SearchProvider = ({ children }) => {
+  const [searchData, setSearchData] = useState(null);
+  return (
+    <SearchContext.Provider value={{ searchData, setSearchData }}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
