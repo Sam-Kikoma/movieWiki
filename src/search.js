dfc2a97e771861fc12c26d93fbeb055ffb345965
Sam Kikoma <kikomasamson@gmail.com>
2024-06-09 16:18:56 +0200

    Updated search function

--- src/search.js
+++ src/search.js
@@ -2,9 +2,6 @@ async function fetchSearch(searchParam) {
   const res = await fetch(
     `http://www.omdbapi.com/?apikey=2f03edc7&t=${searchParam}`,
   );
-  if (!res.ok) {
-    throw new Error("Something went wrong with the search");
-  }
   return res.json();
 }
 
