a6a93f602bd447afe313ec738dd455d9fd524a28
Sam Kikoma <kikomasamson@gmail.com>
2024-06-13 20:36:35 +0200

    Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import React from "react";
 import Navbar from "./components/Navbar";
 import Search from "./components/Search";
 import Card from "./components/Card";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const App = () => {
+  const [searchRes, setSearchRes] = useState(null);
+  const handleSearchData = (data) => {
+    setSearchRes(data);
+  };
+
   return (
     <>
       <Navbar />
-      <Search />
-      <Card />
+      <Search onSearchData={handleSearchData} />
+      {searchRes && <Card searchData={searchRes} />}
     </>
   );
 };
