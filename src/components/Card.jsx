599ec550fcc671fcf31a7177d39d34c2cfba095a
Sam Kikoma <kikomasamson@gmail.com>
2024-06-13 20:35:32 +0200

    Refactored to display api results

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,23 @@
-const Card = () => {
+const Card = ({ searchData }) => {
+  if (!searchData) {
+    return null;
+  }
+  const { Poster, Title, Year, Genre, Plot, Director, Actors, imdbRating } =
+    searchData;
+
   return (
-    <div className="w-[350px] h-[300px]">
+    <div className="w-[350px] h-auto">
       <div className="w-full text-center">
-        <img
-          src="https://images.unsplash.com/photo-1600480505021-e9cfb05527f1?q=80&w=1466&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="Movie image"
-          className="w-[100%] h-auto"
-        />
-        <p>Batman</p>
-        <button>More</button>
+        <img src={Poster} alt={`${Title} poster`} className="w-[100%] h-auto" />
+        <h3>
+          {Title} ({Year})
+        </h3>
+        <p>
+          <strong>Genre:</strong> {Genre}
+        </p>
+        <button className="border border-solid border-black p-2 rounded-md">
+          More
+        </button>
       </div>
     </div>
   );
